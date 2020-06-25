da826c6ae871e098501ff7fd8c68ac973ee976dd
dcashdallas <dcashdallas81@yahoo.com>
2020-06-25 17:04:41 -0500

    updating the seeds values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,40 +1,40 @@
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Salvador Cauli(flower) Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Poblano Picasso Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Hit Me With Your Best Shallot Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("If Looks Could Kale Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Little Swiss Bunshine Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("The Final Kraut Down Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Poutine on the Ritz Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("Onion-tended Consequences Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("A Few Gouda Men Burger", false)
 INSERT INTO burgers
-    (burger_name, devoured, date)
+    (burger_name, devoured)
 VALUES
     ("A Leek of Their Own Burger", false)

2796d543a3244cf77cb5b738e7c086ec946ad4ae
dcashdallas <dcashdallas81@yahoo.com>
2020-06-25 00:57:48 -0500

    updating orm data, establish connection

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,12 @@
 var connection = require("../config/connection.js");
 
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    };
+    console.log('connected as id ' + connection.threadId);
+});
+
 
 module.exports = orm;

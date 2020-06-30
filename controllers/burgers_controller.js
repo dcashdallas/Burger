a8894cadfba616aefc2f60a7d0206704af42fcdd
dcashdallas <dcashdallas81@yahoo.com>
2020-06-29 20:24:06 -0500

    updating controllers, adding create route, devour route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,4 +16,16 @@ router.get('/index', function (req, res) {
     });
 });
 
+router.post('/burger/create', function (req, res) {
+    burger.insertOne(req.body.burger_name, function () {
+        res.redirect('/index');
+    });
+});
+
+router.post('/burger/eat/:id', function (req, res) {
+    burger.updateOne(req.params.id, function () {
+        res.redirect('/index');
+    });
+});
+
 module.exports = router;

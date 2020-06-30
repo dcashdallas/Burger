5db6df8926b041fd54b2486b4b391b31259687bc
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 03:31:20 -0500

    deploying heroku app

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.burger_db) {
 }
 else {
     connection = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'Sublime#96',
-        database: 'burgers_db',
-        port: 3000
+        host: 'us-cdbr-east-02.cleardb.com',
+        user: 'b5b7a8c63bf4fd',
+        password: '1dad4cbf',
+        database: 'heroku_e1c831cb0fcd958',
+
     });
 }
 

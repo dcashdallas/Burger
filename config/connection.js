6794eb7035759d22bfe62ff47eb66c03400bda4e
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 08:15:09 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    port: 3306,
-    user: "b5b7a8c63bf4fd",
-    password: "1dad4cbf",
-    database: "heroku_e1c831cb0fcd958"
-});
+// var connection = mysql.createConnection({
+//     host: "us-cdbr-east-02.cleardb.com",
+//     port: 3306,
+//     user: "b5b7a8c63bf4fd",
+//     password: "1dad4cbf",
+//     database: "heroku_e1c831cb0fcd958"
+// });
 
 // Make connection.
-connection.connect()
+// connection.connect()
 
 // Export connection for our ORM to use.
-module.exports = connection;
+
 
 // function (err) {
 //     if (err) {
@@ -21,3 +21,20 @@ module.exports = connection;
 //     }
 //     console.log("connected as id " + connection.threadId);
 // });
+
+// --user=b5b7a8c63bf4fd --password heroku_e1c831cb0fcd958 --reconnect=true
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Sublime#96',
+        database: 'burgers_db'
+    });
+}
+
+module.exports = connection;

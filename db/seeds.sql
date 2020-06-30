0335e139974f7716b45a7e454586308dbacb1f0e
dcashdallas <dcashdallas81@yahoo.com>
2020-06-29 23:27:55 -0500

    updating final styling, clean up of addtl files

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,37 +2,12 @@ INSERT INTO burgers
     (burger_name, devoured)
 VALUES
     ("Salvador Cauli(flower) Burger", false),
-
-
-
     ("Poblano Picasso Burger", false),
-
-
-
     ("Hit Me With Your Best Shallot Burger", false),
-
-
-
     ("If Looks Could Kale Burger", false),
-
-
-
     ("Little Swiss Bunshine Burger", false),
-
-
-
     ("The Final Kraut Down Burger", false),
-
     ("Poutine on the Ritz Burger", false),
-
-
-
     ("Onion-tended Consequences Burger", false),
-
-
-
     ("A Few Gouda Men Burger", false),
-
-
-
     ("A Leek of Their Own Burger", false)

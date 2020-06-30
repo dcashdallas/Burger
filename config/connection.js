35e8b1da62fe0b65a427eb5d23552bbc3fe86a5e
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 04:20:44 -0500

    deploying heroku app

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.env.burger_db) {
-    connection = mysql.createConnection(process.env.burgers_db);
-}
-else {
-    connection = mysql.createConnection({
-        host: 'us-cdbr-east-02.cleardb.com',
-        user: 'b5b7a8c63bf4fd',
-        password: '1dad4cbf',
-        database: 'heroku_e1c831cb0fcd958',
+var connection = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b5b7a8c63bf4fd',
+    password: '1dad4cbf',
+    database: 'heroku_e1c831cb0fcd958',
+});
 
-    });
-}
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-
-
+// Export connection for our ORM to use.
 module.exports = connection;

42410cf13ac600acb69c82e45c273eff42c12766
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 21:07:12 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
 
-var connection = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    port: 3306,
-    user: "b5b7a8c63bf4fd",
-    password: "1dad4cbf",
-    database: "heroku_e1c831cb0fcd958"
-});
+// var connection = mysql.createConnection({
+//     host: "us-cdbr-east-02.cleardb.com",
+//     port: 3306,
+//     user: "b5b7a8c63bf4fd",
+//     password: "1dad4cbf",
+//     database: "heroku_e1c831cb0fcd958"
+// });
 
 // Make connection.
-connection.connect()
+// connection.connect()
 
 // Export connection for our ORM to use.
-module.exports = connection;
+// module.exports = connection;
 
 // function (err) {
 //     if (err) {
@@ -25,3 +25,26 @@ module.exports = connection;
 
 // --user=b5b7a8c63bf4fd --password heroku_e1c831cb0fcd958 --reconnect=true
 
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Sublime#96",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection.
+module.exports = connection;

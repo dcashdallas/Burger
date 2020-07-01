cfb78e8bfb16ac38deb477197a8f444781cff560
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 19:47:07 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,19 @@
 var mysql = require("mysql");
 
-if (process.env.CLEARDB_DATABASE_URL) {
-    // DB is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-} else {
 
-    connection = mysql.createConnection({
-        port: '3306',
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'burgers_db'
-    });
+var connection = mysql.createConnection({
+    host: "us-cdbr-east-02.cleardb.com",
+    port: 3306,
+    user: "b5b7a8c63bf4fd",
+    password: "1dad4cbf",
+    database: "heroku_e1c831cb0fcd958"
+});
 
-    connection.connect(function (error) {
-        if (error) {
-            console.error('Connection error: ' + error.stack);
-            return;
-        }
-        console.log('MySQL connection established! Connected as ID ' + connection.threadId);
-    });
+// Make connection.
+connection.connect()
 
-    // Export connection for our ORM to use.
-    module.exports = connection;
+// Export connection for our ORM to use.
+module.exports = connection;
 
 // function (err) {
 //     if (err) {
--- server.js
+++ server.js
@@ -18,5 +18,12 @@ app.use('/', router);
 
 
 
-var port = process.env.PORT || 3207;
-app.listen(port);
+// var port = process.env.PORT || 3207;
+// app.listen(port);
+
+app.set("port", (process.env.PORT || 3000));
+
+// app.use('/', routes);
+app.listen(app.get("port"), function () {
+    console.log("You are running on port", app.get("port"));
+});

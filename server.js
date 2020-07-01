fa985844da5f409ff23c9ae7a135eceb6c260d7f
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 20:08:54 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -21,15 +21,10 @@ app.use('/', router);
 // var port = process.env.PORT || 3207;
 // app.listen(port);
 
-app.set("port", (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 2001));
 
 // app.use('/', routes);
-// app.listen(app.get("port"), function () {
-//     console.log("You are running on port", app.get("port"));
-// });
-
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+app.listen(app.get("port"), function () {
+    console.log("You are running on port", app.get("port"));
 });
+

4a3af5338e3f6817056c40ba8e4e8ae2409aa4c2
dcashdallas <dcashdallas81@yahoo.com>
2020-06-30 19:27:54 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    port: 3306,
-    user: "b5b7a8c63bf4fd",
-    password: "1dad4cbf",
-    database: "heroku_e1c831cb0fcd958"
-});
+if (process.env.ClearDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.Cleardb_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "us-cdbr-east-02.cleardb.com",
+        port: 3306,
+        user: "b5b7a8c63bf4fd",
+        password: "1dad4cbf",
+        database: "heroku_e1c831cb0fcd958"
+    });
 
-// Make connection.
-connection.connect()
+    // Make connection.
+    connection.connect()
 
-// Export connection for our ORM to use.
-module.exports = connection;
+    // Export connection for our ORM to use.
+    module.exports = connection;
 
 // function (err) {
 //     if (err) {
